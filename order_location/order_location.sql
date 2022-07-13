7c9a83311a2b2879fd70af3a4cf858a8b1e6da48
gobr2005 <83517458+gobr2005@users.noreply.github.com>
2022-07-13 16:40:25 -0400

    Updated format
    I updated the format of the code to make it easier to read. I also added comments to add clarity to the code.

--- order_location/order_location.sql
+++ order_location/order_location.sql
@@ -1,23 +1,29 @@
 WITH
   order_address AS (
-  SELECT
-    addresses.order_id,
-    addresses.city,
-    spc.state,
-    LEFT(addresses.postal_code, 5) AS zip,
-    addresses.country_code
-  FROM
-    `project_name.store_name.bc_order_shipping_addresses` AS addresses
-  JOIN
-    `project_name.store_name.state_postal_code` AS spc
-  ON
-    CASE
-      WHEN LENGTH(addresses.state) = 2 THEN addresses.state = spc.state_code
-    ELSE
-    addresses.state = spc.state
-  END
-  WHERE
-    country_code = 'US' )
+    /* 
+       This subquery serves many purposes. First, it reduces to the zip code to the first 5 characters. The helps connected the same zip code when a customer 
+       uses the Zip+4 code. Second, somes customers uses a state abbreviation so a new table was created to connect the abbreviation with the state name. The join 
+       looks at the length of character in the state then joins based which column. For example, if the state was NE, then the join was between state and state_code. 
+       However, if the state was Nebraska, it would join state and state. I also limited the query to the United States. 
+    */
+    SELECT
+      addresses.order_id,
+      addresses.city,
+      spc.state,
+      LEFT(addresses.postal_code, 5) AS zip, --This makes all the zip codes uniformed. 
+      addresses.country_code
+    FROM
+      `project_name.store_name.bc_order_shipping_addresses` AS addresses
+    JOIN
+      `project_name.store_name.state_postal_code` AS spc --This is a separate table created by uploading a CSV into BigQuery
+    ON
+      CASE
+        WHEN LENGTH(addresses.state) = 2 THEN addresses.state = spc.state_code 
+        ELSE addresses.state = spc.state
+      END
+    WHERE
+      country_code = 'US' 
+  )
 SELECT
   orders.order_id,
   trim(order_address.city) city,
@@ -31,6 +37,9 @@ JOIN
 ON
   orders.order_id = order_address.order_id
 WHERE
+  /*
+  This Where statement limits the query to orders that money was exchanged. Refunded was excluded since all products were returned.
+  */
   order_status = "Completed"
   OR order_status = "Awaiting Shipment"
   OR order_status = "Partially Shipped"
